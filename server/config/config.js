613a3f626593a44751e905f436c92138780b27f0
unknown <r.loboplaza@hotmail.com>
2021-07-25 09:43:02 -0300

    agregando config

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+//Port
+process.env.PORT = process.env.PORT || 3004;
+
+//La firma del token
+process.env.SEED = process.env.SEED || 'esta_es_la_firma';
+
+//La fecha de expiracion del token
+process.env.EXPIRACION = '48h';
